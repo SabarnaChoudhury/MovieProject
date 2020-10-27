da1c434da5a0d26697c66aae54e2d7d4768e5de6
udit <dude.udit32@gmail.com>
2020-10-27 16:58:43 +0530

    login/sign up updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbdDropdownForm } from './dropdown-form/dropdown-form.component';
-import { SignupPageComponent } from './signup-page/signup-page.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NgbdDropdownForm,
-    SignupPageComponent
+    NgbdDropdownForm
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
